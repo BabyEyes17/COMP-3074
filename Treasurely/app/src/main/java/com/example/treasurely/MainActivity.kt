0f0582f741c244b30701c01abdae1b1fdf71a862
Jayden Lewis <jaydent.lewis@gmail.com>
2025-10-02 16:54:52 -0400

    Built a nav bar, continuing work on dashboard

--- Treasurely/app/src/main/java/com/example/treasurely/MainActivity.kt
+++ Treasurely/app/src/main/java/com/example/treasurely/MainActivity.kt
@@ -5,30 +5,25 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.treasurely.ui.theme.TreasurelyTheme
+import kotlinx.coroutines.launch
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,15 +31,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TreasurelyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
-                    Column(modifier = Modifier
-                        .fillMaxSize()
-                        .padding(innerPadding),
-                        verticalArrangement = Arrangement.Bottom,
-                        horizontalAlignment = Alignment.CenterHorizontally) {
+                var selectedScreen by remember { mutableIntStateOf(0) }
 
-                        NavBar()
+                Scaffold(
+                    bottomBar = { NavBar(selectedScreen) { selectedScreen = it } }
+                )
+                {
+                    innerPadding -> Box(
+
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    )
+                    {
+                        when (selectedScreen) {
+                            0 -> Dashboard()
+                            1 -> QRCodeScanner()
+                            2 -> ClueList()
+                            3 -> JoinTreasureHunt()
+                            4 -> CreateTreasureHunt()
+                        }
                     }
                 }
             }
@@ -52,41 +60,138 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
-fun NavBar() {
+fun NavBar(selectedScreen: Int, onSelect: (Int) -> Unit) {
+
+    val context = LocalContext.current
 
     NavigationBar {
 
-        val context = LocalContext.current
-        var selectedIndex by remember { mutableIntStateOf(-1) }
-
         NavigationBarItem(
-            icon = { Icon(painter = painterResource(id = R.drawable.outline_qr_code_scanner_24), contentDescription = "Scan QR Code") },
-            //label = { Text("Create") },
-            selected = selectedIndex == 1,
+            icon = { Icon(painter = painterResource(id = R.drawable.outline_qr_code_scanner_24), contentDescription = "Scan QR Code", modifier = Modifier.size(32.dp)) },
+            selected = selectedScreen  == 1,
             onClick = {
-                selectedIndex = 1
-                Toast.makeText(context, "Hey There!", Toast.LENGTH_SHORT).show()}
+                onSelect(1)
+                Toast.makeText(context, "Hey There!", Toast.LENGTH_SHORT).show()
+
+            }
         )
 
         NavigationBarItem(
-            icon = { Icon(Icons.Default.Home, contentDescription = "Home") },
-            //label = { Text("Home") },
-            selected = selectedIndex == 0,
+            icon = { Icon(painter = painterResource(id = R.drawable.baseline_question_mark_24), contentDescription = "Clues", modifier = Modifier.size(32.dp)) },
+            selected = selectedScreen  == 2,
             onClick = {
-                selectedIndex = 0
-                Toast.makeText(context, "Hey There!", Toast.LENGTH_SHORT).show()}
+                onSelect(2)
+                Toast.makeText(context, "Hey There!", Toast.LENGTH_SHORT).show()
+
+            }
         )
 
         NavigationBarItem(
-            icon = { Icon(Icons.Default.AddCircle, contentDescription = "Create Treasure Hunt") },
-            //label = { Text("Create") },
-            selected = selectedIndex == 2,
+            icon = { Icon(Icons.Default.Home, contentDescription = "Dashboard", modifier = Modifier.size(32.dp)) },
+            selected = selectedScreen  == 0,
             onClick = {
-                selectedIndex = 2
-                Toast.makeText(context, "Hey There!", Toast.LENGTH_SHORT).show()}
+                onSelect(0)
+                Toast.makeText(context, "Hey There!", Toast.LENGTH_SHORT).show()
+
+            }
         )
 
+        NavigationBarItem(
+            icon = { Icon(painter = painterResource(id = R.drawable.outline_group_24), contentDescription = "Join Treasure Hunt", modifier = Modifier.size(32.dp)) },
+            selected = selectedScreen  == 3,
+            onClick = {
+                onSelect(3)
+                Toast.makeText(context, "Hey There!", Toast.LENGTH_SHORT).show()
 
+            }
+        )
+
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.AddCircle, contentDescription = "Create Treasure Hunt", modifier = Modifier.size(32.dp)) },
+            selected = selectedScreen  == 4,
+            onClick = {
+                onSelect(4)
+                Toast.makeText(context, "Hey There!", Toast.LENGTH_SHORT).show()
+
+            }
+        )
     }
+}
+
+
+@Composable
+fun QRCodeScanner() {
+    val context = LocalContext.current
+
+    Box(modifier = Modifier.fillMaxSize()) {
+
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.outline_qr_code_24),
+                contentDescription = "Temporary QR Code",
+                modifier = Modifier.size(200.dp)
+            )
+        }
+
+        Button(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 16.dp)
+                .width(160.dp),
+
+            onClick = { Toast.makeText(context, "Scanning...", Toast.LENGTH_SHORT).show() }
+        )
+        {
+            Text("Scan", fontSize = 20.sp)
+        }
+    }
+}
+
+
+@Composable
+fun ClueList() {
+
+    Text("Clue List")
+}
+
+
+/*
+* The dashboard should contain:
+*   - Any active treasure hunts the user is a part of
+*/
+@Composable
+fun Dashboard() {
+    val hunts = listOf(
+        "Casa Loma Campus Hunt",
+        "TTC Subway Treasure Hunt",
+    )
+
+
+}
+
+
+/*
+* The join screen should contain:
+*   - An input box for a 6 digit code (corresponds with a specific hunt)
+*   - A submit button that if correct, transfers you to the dashboard
+*/
+@Composable
+fun JoinTreasureHunt() {
+
+    Text("Join Treasure Hunt")
+}
+
+
+@Composable
+fun CreateTreasureHunt() {
+
+    Text("Create Treasure Hunt")
 }
