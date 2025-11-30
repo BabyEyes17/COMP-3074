4ef5f281a1adf27f22d67af003bd3e17e205ed3a
Jayden Lewis <jaydent.lewis@gmail.com>
2025-11-30 12:25:37 -0500

    Added type converters for List <-> String conversion

--- app/src/main/java/com/example/treasurely/data/Converters.kt
+++ app/src/main/java/com/example/treasurely/data/Converters.kt
@@ -1,4 +1,14 @@
 package com.example.treasurely.data
 
+import androidx.room.TypeConverter
+
 class Converters {
+
+    @TypeConverter
+    fun fromList(list: List<String>): String =
+        list.joinToString(",")
+
+    @TypeConverter
+    fun toList(data: String): List<String> =
+        if (data.isBlank()) emptyList() else data.split(",")
 }
